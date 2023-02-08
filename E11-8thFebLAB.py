ab699f560a8d1d12cb36d4a1676109d477784b5f
noilboy <124214248+noilboy@users.noreply.github.com>
2023-02-08 15:50:12 -0800

    Create E11-8thFebLAB.py

---
+++ E11-8thFebLAB.py
@@ -0,0 +1,30 @@
+import time
+import board
+import busio
+from digitalio import DigitalInOut, Direction, Pull
+from adafruit_pm25.i2c import PM25_I2C
+
+
+import serial
+uart = serial.Serial("/dev/ttyS0", baudrate=9600, timeout=0.25)
+
+# Connect to a PM2.5 sensor over UART
+from adafruit_pm25.uart import PM25_UART
+pm25 = PM25_UART(uart, reset_pin)
+
+# Create library object, use 'slow' 100KHz frequency!
+i2c = busio.I2C(board.SCL, board.SDA, frequency=100000)
+# Connect to a PM2.5 sensor over I2C
+pm25 = PM25_I2C(i2c, reset_pin)
+
+print("Found PM2.5 sensor, reading data...")
+
+while True:
+    time.sleep(1)
+
+    try:
+        aqdata = pm25.read()
+        print(aqdata)
+    except RuntimeError:
+        print("Unable to read from sensor, retrying...")
+        continue
