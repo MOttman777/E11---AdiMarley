ed571bb2e12f987c36a8720841066e79216353fd
noilboy <124214248+noilboy@users.noreply.github.com>
2023-02-08 16:00:22 -0800

    Update E11-8thFebLAB.py

--- E11-8thFebLAB.py
+++ E11-8thFebLAB.py
@@ -5,6 +5,12 @@ from digitalio import DigitalInOut, Direction, Pull
 from adafruit_pm25.i2c import PM25_I2C
 
 
+reset_pin = None
+#If you have a GPIO, its not a bad idea to connect it to the RESET pin
+reset_pin = DigitalInOut(board.G0)
+reset_pin.direction = Direction.OUTPUT
+reset_pin.value = False
+
 import serial
 uart = serial.Serial("/dev/ttyS0", baudrate=9600, timeout=0.25)
 
@@ -13,9 +19,9 @@ from adafruit_pm25.uart import PM25_UART
 pm25 = PM25_UART(uart, reset_pin)
 
 # Create library object, use 'slow' 100KHz frequency!
-i2c = busio.I2C(board.SCL, board.SDA, frequency=100000)
+#i2c = busio.I2C(board.SCL, board.SDA, frequency=100000)
 # Connect to a PM2.5 sensor over I2C
-pm25 = PM25_I2C(i2c, reset_pin)
+#pm25 = PM25_I2C(i2c, reset_pin)
 
 print("Found PM2.5 sensor, reading data...")
 
