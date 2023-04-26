b88342097c2ee468784f5973830f6852d887a4bc
MOttman777 <ottman@berkeley.edu>
2023-04-26 15:15:49 -0700

    Create accel2.py

---
+++ accel2.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import time
+import board
+import busio
+#import adafruit_adxl34x
+import digitalio
+import adafruit_lis3dh
+import time
+import argparse
+
+
+# In[ ]:
+
+
+i2c = busio.I2C(board.SCL, board.SDA)
+#i2c = board.I2C()
+#accelerometer = adafruit_adxl34x.ADXL345(i2c)
+accelerometer = adafruit_lis3dh.LIS3DH_I2C(i2c, int1=int1)
+
+import csv
+meta_data = ["X","Y","Z"]
+f = open(str(sys.argv[2]),"w",newline = '')
+writer = csv.writer(f)
+writer.writerow(meta_data)
+print(sys.argv)
+start_time = int(time.time())
+run_time = int(sys.argv[1])
+itime = start_time
+
+while itime < (start_time + run_time):
+    print("%f %f %f"%accelerometer.acceleration)
+    itime = int(time.time())
+    try:
+        accdata = accel.read()
+    except RuntimeError:
+        print("Unable to read from sensor, retrying...")
+        continue
+    data = [accdata[0],accdata[1],accdata[2]]
+    writer.writerow(data)
+f.close() 
+time.sleep(1)
+
